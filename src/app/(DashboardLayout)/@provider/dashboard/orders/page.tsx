fc9ba3314e1d8008fd20a05389c9de2a87629005
abubakar308 <mdabubakarsiddique789@gmail.com>
2026-02-10 21:02:46 +0600

    provider order get ui and function created

---
+++ src/app/(DashboardLayout)/@provider/dashboard/orders/page.tsx
@@ -0,0 +1,12 @@
+import ProviderOrder from "@/components/provider/Orders";
+
+
+const page = () => {
+    return (
+        <div>
+           <ProviderOrder />
+        </div>
+    );
+};
+
+export default page;
